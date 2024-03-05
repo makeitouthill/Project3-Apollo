6e7c43af56cab72236a8307e5d40132e7b814139
AlexH <>
2024-03-04 19:57:23 -0800

    Initial commit for Staking Contract, Importing needed inhertiance files

---
+++ contracts/StakingContract.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
