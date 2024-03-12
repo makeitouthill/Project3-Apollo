02fe9a151019d04b160d9286bd7ce1bd48ff5730
AlexH <>
2024-03-11 19:50:50 -0700

    Was able to deploy marketplace contract onto a Hardhat local testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,27 @@
 const { ethers } = require("hardhat");
+const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
   const [deployer] = await ethers.getSigners();
-  const balance = await ethers.provider.getBalance(deployer.address);
-  console.log(`Deploying contracts with the account: ${deployer.address}`);
-  console.log(`Account balance: ${ethers.utils.formatEther(balance)} ETH`);
-
-  const Marketplace = await ethers.getContractFactory("NFTMarketplace");
+  const balance = await deployer.getBalance();
+  const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
   const marketplace = await Marketplace.deploy();
 
   await marketplace.deployed();
-  console.log(`Contract deployed to address: ${marketplace.address}`);
 
   const data = {
     address: marketplace.address,
-    abi: marketplace.interface.format('json')
-  };
-
-  // Ensure the ./src directory exists or handle the error
-  if (!fs.existsSync('./src')) {
-    fs.mkdirSync('./src', { recursive: true });
+    abi: JSON.parse(marketplace.interface.format('json'))
   }
-  
-  // This writes the ABI and address to the mktplace.json
-  fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data, null, 2)); // Pretty print JSON
+
+  //This writes the ABI and address to the mktplace.json
+  fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error("Error:", error);
+    console.error(error);
     process.exit(1);
   });
