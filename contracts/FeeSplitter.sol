0b982e7a993bb34dcc67013f255aad4049a740b5
AlexH <>
2024-03-11 17:13:53 -0700

    Hardhat/Ethereum Sepolia Testnet Deployment(Working Progress)

--- contracts/FeeSplitter.sol
+++ contracts/FeeSplitter.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IApolloStaking {
-	function notifyNewRewards (uint256 amount) external
+	function notifyNewRewards (uint256 amount) external;
 }
 
 contract FeeSplitter is ReentrancyGuard, Ownable {
